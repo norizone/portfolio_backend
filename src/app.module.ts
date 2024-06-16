9097c48207c52f5d363b26a53424c5b9c41c20d5
minami <minami.takanori.1113@gmail.com>
2024-06-16 17:19:27 +0900

    file作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { WorkModule } from './work/work.module';
+import { AdminModule } from './admin/admin.module';
+import { ToolModule } from './tool/tool.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule, WorkModule, AdminModule, ToolModule, PrismaModule, AuthModule],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
