50f17c415c1c71cb35d6d80cf766086796d66177
minami <minami.takanori.1113@gmail.com>
2024-06-23 13:47:55 +0900

    user対応

--- src/admin/user/user.controller.ts
+++ src/admin/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Msg } from './interfaces/user.interface';
 
-@Controller('user')
-export class UserController {}
+@UseGuards(AuthGuard('jwtAdmin'))
+@Controller('admin/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('create')
+  createUser(@Body() dto: CreateUserDto): Promise<Msg> {
+    return this.userService.createUser(dto);
+  }
+}
--- src/admin/user/user.service.ts
+++ src/admin/user/user.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Msg } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(dto: CreateUserDto): Promise<Msg> {
+    const { permission, email, password } = dto;
+    const hashed = await bcrypt.hash(password, 12);
+    try {
+      await this.prisma.user.create({
+        data: {
+          email,
+          hashedPassword: hashed,
+          permission: permission,
+        },
+      });
+      return {
+        message: 'ok',
+      };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException(
+            'このメールアドレスはすでに使用されています。',
+          );
+        }
+      }
+      throw error;
+    }
+  }
+}
