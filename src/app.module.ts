6c7e21bfaae5ee680064daf9337309adff839129
minami <minami.takanori.1113@gmail.com>
2024-07-01 08:24:13 +0900

    画像テスト

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AdminModule } from './admin/admin.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { FrontModule } from './front/front.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -12,6 +14,10 @@ import { FrontModule } from './front/front.module';
     PrismaModule,
     AdminModule,
     FrontModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '/src/uploads'), // 静的ファイルのディレクトリを指定
+      serveRoot: '/uploads', // URLパスのルートを指定
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
