562b0919463b27e0967e5bad8aea6d33bbac6356
minami <minami.takanori.1113@gmail.com>
2024-08-07 10:02:25 +0900

    cookie変更

--- src/admin/auth/auth.controller.ts
+++ src/admin/auth/auth.controller.ts
@@ -36,7 +36,7 @@ export class AuthController {
     res.cookie('access_token', '', {
       httpOnly: true,
       secure: true,
-      sameSite: 'lax',
+      sameSite: 'none',
       path: '/',
     });
     return {
@@ -54,7 +54,7 @@ export class AuthController {
     res.cookie('access_token', jwt.accessToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'lax',
+      sameSite: 'none',
       path: '/',
     });
     return {
