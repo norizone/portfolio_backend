98b4454557219a4977056dba1e881b49429971eb
minami <minami.takanori.1113@gmail.com>
2024-08-09 06:49:24 +0900

    配列でoriginを渡す

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
-// import { Request } from 'express';
 import * as cookieParser from 'cookie-parser';
 import { NestExpressApplication } from '@nestjs/platform-express';
-// import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
   // グローバルにValidationPipeを適用し、whitelistでDTOにないプロパティを削除
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     //CORS設定
     credentials: true, //cookie受け取るよう
-    origin: [process.env.ORIGIN_ADMIN, process.env.ORIGIN_FRONT],
+    origin: allowedOrigins,
   });
   app.use(cookieParser());
   await app.listen(process.env.PORT || 3005);
